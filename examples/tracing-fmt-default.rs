346dcc35eec5d059d179e5ec95e703371e866546
YANG Qi <y3tothe6@outlook.com>
2025-09-07 15:11:04 +0800

    Update

--- examples/tracing-fmt-default.rs
+++ examples/tracing-fmt-default.rs
@@ -1,5 +1,5 @@
 use clap::{Parser, ValueEnum};
-use tracing_subscriber::fmt::time::ChronoLocal;
+use tracing_subscriber::fmt::time::{ChronoLocal, OffsetTime};
 use u3::tracing::fmt_default::WithU3Default;
 
 #[derive(Parser)]
@@ -12,6 +12,7 @@ struct Args {
 enum Baseline {
     A,
     B,
+    C,
 }
 
 fn main() {
@@ -19,6 +20,9 @@ fn main() {
         None => tracing_subscriber::fmt().with_u3_default().init(),
         Some(Baseline::A) => tracing_subscriber::fmt().init(),
         Some(Baseline::B) => tracing_subscriber::fmt().with_timer(ChronoLocal::rfc_3339()).init(),
+        Some(Baseline::C) => {
+            tracing_subscriber::fmt().with_timer(OffsetTime::local_rfc_3339().unwrap()).init()
+        }
     }
     (0..100).for_each(|_| tracing::info!("info"));
 }
