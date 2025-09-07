e71f3be379e403e2e9fdfb825f524a83939a2adb
YANG Qi <y3tothe6@outlook.com>
2025-09-07 15:35:29 +0800

    Update

--- src/tracing/fmt_default.rs
+++ src/tracing/fmt_default.rs
@@ -1,5 +1,9 @@
 use chrono::prelude::*;
-use tracing_subscriber::fmt::{FormatFields, SubscriberBuilder, format::Format, time::FormatTime};
+use tracing_subscriber::fmt::{
+    FormatFields, SubscriberBuilder,
+    format::{Format, Writer},
+    time::FormatTime,
+};
 
 pub trait WithU3Default<N, L, F, W> {
     fn with_u3_default(self) -> SubscriberBuilder<N, Format<L, Timer>, F, W>;
@@ -17,7 +21,7 @@ where
 pub struct Timer;
 
 impl FormatTime for Timer {
-    fn format_time(&self, w: &mut tracing_subscriber::fmt::format::Writer<'_>) -> std::fmt::Result {
+    fn format_time(&self, w: &mut Writer<'_>) -> std::fmt::Result {
         write!(w, "{}", Local::now().to_rfc3339_opts(SecondsFormat::Micros, false))
     }
 }
