18063463e215e3bc3017ddcb34cca1921bf898f1
YANG Qi <y3tothe6@outlook.com>
2025-09-08 22:23:34 +0800

    Update

--- src/chrono/ext.rs
+++ src/chrono/ext.rs
@@ -1,14 +1,24 @@
-use chrono::prelude::*;
+use std::fmt::Display;
+
+use chrono::{
+    format::{DelayedFormat, StrftimeItems},
+    prelude::*,
+};
 
 pub trait Ext {
     fn format_u3(&self) -> String;
+    fn format_for_filename(&self) -> DelayedFormat<StrftimeItems<'static>>;
 }
 
 impl<Tz> Ext for DateTime<Tz>
 where
     Tz: TimeZone,
+    Tz::Offset: Display,
 {
     fn format_u3(&self) -> String {
         self.to_rfc3339_opts(SecondsFormat::Micros, false)
+    }
+    fn format_for_filename(&self) -> DelayedFormat<StrftimeItems<'static>> {
+        self.format("%Y%m%d-%H%M%S-%6f")
     }
 }
